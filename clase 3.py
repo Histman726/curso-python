b64f080d14bc71cd80b3fb4fbf1e017cb0fb03a4
Histman7 <hsandovalt1900@alumno.ipn.mx>
2022-09-01 10:53:55 -0500

    Ejemplo de match (switch en otros lenguajes)

--- clase 3.py
+++ clase 3.py
@@ -1,7 +1,17 @@
-color="rojo"
-forma="circulo"
+error=input("Introduzca el error:\n")
 
-if color=="rojo" and forma=="circulo":
-    print("Es un circulo rojo")
-else:
-    print("No se cumple con las condiciones")
+match error:
+    case "200":
+        print("Todo OK")
+    case "301":
+        print("Movimiento permanente en la pagina")
+    case "302":
+        print("Movimiento temporal en la pagina")
+    case "404":
+        print('Pagina no encontrada')
+    case "500":
+        print('Error interno en el servidor')
+    case "503":
+        print("Servicio no disponible")
+    case _:
+        print("No hay errores")
