3c133a963382cad89d64ec72296d61adc7af90f6
Histman7 <hsandovalt1900@alumno.ipn.mx>
2022-09-01 10:44:49 -0500

    Ejemplo de or y and en un condicional

--- clase 3.py
+++ clase 3.py
@@ -1,21 +1,7 @@
-edad=int(input("Introduzca su edad:\n"))
+color="rojo"
+forma="circulo"
 
-# Se deja la variable sin valor
-respuesta=None
-
-if edad>=18:
-    print("Eres mayor de edad. Eliga una opcion\n")
-    respuesta=input("\t1. ron\n\t2. tequila\n\t3. whisky\n\t4. vino\n")
-
-    if respuesta=="1":
-        print("Usted ha eligido tomar ron")
-    elif respuesta=="2":
-        print("Usted ha eligido tomar tequila")
-    elif respuesta=="3":
-        print("Usted ha eligido tomar whisky")
-    elif respuesta=="4":
-        print("Usted ha eligido tomar vino")
-    else:
-        print("Su opcion no es valida")
+if color=="rojo" and forma=="circulo":
+    print("Es un circulo rojo")
 else:
-    print("Usted es menor de edad no puede tomar alcohol1")
+    print("No se cumple con las condiciones")
